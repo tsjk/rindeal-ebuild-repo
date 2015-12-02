65fb550240aa9fc1ad8978b0e57f5a4bde50b86e
Jan Chren <dev.rindeal@outlook.com>
2015-12-02 13:04:19 +0100

    README added

---
+++ README.md
@@ -0,0 +1,3 @@
+In each category/package directory you should find _README_ file describing why it's included in this overlay.
+
+For package listing see [LISTING.md](./tree/master/LISTING.md).
---
+++ build_listing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+URL_PREFIX="./tree/master"
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+for cat in *-*/; do
+    cat="${cat%/}"
+    echo "- [$cat]($URL_PREFIX/$cat)"
+
+    pushd $cat/ >/dev/null 2>&1
+    for pn in */; do
+        pn="$pn"
+        echo " - [$pn]($URL_PREFIX/$cat/$pn)"
+    done
+    popd >/dev/null 2>&1
+
+done > LISTING.md
