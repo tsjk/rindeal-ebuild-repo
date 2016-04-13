7fd1e588f8a954a94aaa9c9fed685cc1fab59a73
Jan Chren <dev.rindeal@gmail.com>
2016-04-13 02:29:49 +0200

    travis: travis-functions.sh: cleanup + doc

--- .travis/travis-functions.sh
+++ .travis/travis-functions.sh
@@ -7,30 +7,35 @@ announce() {
 }
 
 travis_retry() {
-  local result=0
-  local count=1
-  while [ $count -le 3 ]; do
-    [ $result -ne 0 ] && {
-      echo -e "\n\033[33;1mThe command \"$@\" failed. Retrying, $count of 3.\033[0m\n" >&2
-    }
+  local result=0 count=3
+  while ((count--)) ; do
     "$@"
     result=$?
-    [ $result -eq 0 ] && break
-    count=$(($count + 1))
+
+    if [ $result -eq 0 ] ; then
+        break
+    else
+      echo -e "\n\033[33;1mThe command \"$@\" failed. Retrying, $count of 3.\033[0m\n" >&2
+    fi
+
     sleep 1
   done
 
-  [ $count -eq 3 ] && {
+  [ $count -le 0 ] && {
     echo "\n\033[33;1mThe command \"$@\" failed 3 times.\033[0m\n" >&2
   }
 
   return $result
 }
 
+# Usage: fold_start <TAG> <COMMENT>
+#
+# These statements can be nested and must be in pair with `fold_end()`.
 fold_start() {
-  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+  echo -e "travis_fold:start:${1}\033[33;1m${2}\033[0m"
 }
 
+# Usage: fold_end <TAG>
 fold_end() {
-  echo -e "\ntravis_fold:end:$1\r"
+  echo -e "\ntravis_fold:end:${1}\r"
 }
