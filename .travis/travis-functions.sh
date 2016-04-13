d689c4d091b44754be5d363b3fb9d45f09a2fd45
Jan Chren <dev.rindeal@gmail.com>
2016-04-13 02:03:59 +0200

    travis: add `travis-functions.sh`

---
+++ .travis/travis-functions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+# https://github.com/travis-ci/travis-rubies
+
+announce() {
+  echo \$ $@
+  $@
+}
+
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n\033[33;1mThe command \"$@\" failed. Retrying, $count of 3.\033[0m\n" >&2
+    }
+    "$@"
+    result=$?
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -eq 3 ] && {
+    echo "\n\033[33;1mThe command \"$@\" failed 3 times.\033[0m\n" >&2
+  }
+
+  return $result
+}
+
+fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
