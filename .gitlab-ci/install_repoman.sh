2a553eaf582b9a2e183379f31ea294420c3ad89c
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2016-09-23 19:57:56 +0200

    accept_keywords

--- .gitlab-ci/install_repoman.sh
+++ .gitlab-ci/install_repoman.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat <<-'_EOF_' >> /etc/portage/package.accepted_keywords
+cat <<-'_EOF_' >> /etc/portage/package.accept_keywords
     # required by app-portage/repoman-2.3.0-r1::gentoo
     # required by repoman (argument)
     =dev-python/lxml-3.6.4 ~amd64
