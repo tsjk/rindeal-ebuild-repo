8a00a500f5e60727f5cfa055475ea1ffd7f0dd77
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2016-09-23 20:38:44 +0200

    fix portage 2

--- .gitlab-ci/install_repoman.sh
+++ .gitlab-ci/install_repoman.sh
@@ -6,13 +6,13 @@ set -x
 cat <<-'_EOF_' >> /etc/portage/package.accept_keywords
     # required by app-portage/repoman-2.3.0-r1::gentoo
     # required by repoman (argument)
-    =dev-python/lxml-3.6.4 ~amd64
+    ~dev-python/lxml-3.6.4 ~amd64
     # required by =sys-apps/portage-2.3.0 (argument)
-    =sys-apps/portage-2.3.0 ~amd64
+    ~sys-apps/portage-2.3.0 ~amd64
     # required by repoman (argument)
-    =app-portage/repoman-2.3.0-r1 ~amd64
+    ~app-portage/repoman-2.3.0 ~amd64
 _EOF_
 
 eselect profile set default/linux/amd64/13.0/systemd
 
-emerge --ask=n repoman
+emerge --ask=n portage repoman
