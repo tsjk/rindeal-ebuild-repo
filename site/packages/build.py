b02604d678ab4682a91bd01d1d1d356f584d782b
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2016-10-14 01:22:07 +0200

    site/packages: dfsd

--- site/packages/build.py
+++ site/packages/build.py
@@ -7,9 +7,25 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 fs_loader = jinja2.FileSystemLoader(SCRIPT_DIR + '/templates')
 jinja_env = jinja2.Environment(
-        loader=fs_loader,
-        autoescape=true
+        loader = fs_loader,
+        autoescape = True,
+        trim_blocks = True,
+        lstrip_blocks = True
     )
 
-template = jinja_env.get_template('mytemplate.html')
-template.render(name='John Doe')
+template = jinja_env.get_template('pages/index.html')
+print(template.render(name='John Doe'))
+
+# find PORTDIR_OVERLAY
+dir = SCRIPT_DIR+"/foo"
+while len(dir):
+    dir = os.path.dirname(dir)
+    if not os.path.isfile(dir + "/metadata/layout.conf"):
+        continue
+    PORTDIR_OVERLAY = dir
+    break
+
+os.environ["PORTDIR_OVERLAY"]=PORTDIR_OVERLAY
+
+import portage
+portage_db = portage.db[portage.root]["porttree"].dbapi
